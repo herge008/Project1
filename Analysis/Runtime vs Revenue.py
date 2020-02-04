a823660c6a739653ab89a2730efcfddb9d83938e
Chloe Zhao <chloezhao@Chloes-MacBook-Air-2.local>
2020-02-04 13:01:07 -0500

    Final Version

--- Analysis/Runtime vs Revenue.py
+++ Analysis/Runtime vs Revenue.py
@@ -3,6 +3,7 @@
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as st
 import numpy as np
 import os
 
@@ -55,15 +56,20 @@ sort_time
 
 # %%
 # Organize movie runtimes to bins
-bins = [0, 60, 90, 120, 180, 240]
-group_names = ["0-60mins", "60-90mins", "90-120mins", "120-180mins", "180-240mins"]
-colors = ["red", "yellow", "blue", "green", "orange"]
+bins = [60, 90, 120, 180, 240]
+group_names = ["60-90mins", "91-120mins", "121-180mins", "181-240mins"]
+colors = ["red", "blue", "green", "orange"]
 sort_time["Movies Runtime Grouped(Mins)"] = pd.cut(sort_time["Runtime (Min)"], bins, labels=group_names)
 sort_time
-                                            
 
 
 # %%
+# Organize movie runtimes to bins
+bins = [60, 90, 120, 180, 240]
+group_names = ["60-90mins", "91-120mins", "121-180mins", "181-240mins"]
+colors = ["red", "blue", "green", "orange"]
+sort_time["Movies Runtime Grouped(Mins)"] = pd.cut(sort_time["Runtime (Min)"], bins, labels=group_names)
+
 # Create a bar plot to show most common movie run time
 time_counts = sort_time["Movies Runtime Grouped(Mins)"].value_counts(sort=False)
 df_val_counts = pd.DataFrame(time_counts)
@@ -72,11 +78,11 @@ df_val_counts.columns = ['Runtime Grouped', 'Counts']
 print(df_val_counts)
 
 
-colors = ["red", "orange", "green", "blue", "yellow"]
-explode = (0.5, 0, 0.2, 0, 0)
-fig = plt.figure()
+colors = ["red", "orange", "green", "blue"]
+explode = (0.2, 0, 0.2, 0.2)
+fig = plt.figure(figsize=(10, 7))
 plt.pie(df_val_counts["Counts"], explode=explode, labels=group_names, colors=colors,
-        autopct="%1.1f%%", shadow=True, startangle=140, radius=1)
+        autopct="%1.1f%%", shadow=False, startangle=170, radius=1)
 plt.title("Movie Runtime (2008-2018)")
 plt.tight_layout()
 
@@ -91,18 +97,47 @@ fig.savefig(save_file)
 x = sort_time["Runtime (Min)"]
 y = sort_time["Revenue (Millions)"]
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10, 7))
 plt.scatter(x, y)
+(slope, intercept, rvalue, pvalue, stderr) = st.linregress(x, y)
+regress_values = x * slope + intercept
+plt.plot(x, regress_values, "r-")
 plt.xlabel("Movie Runtime (Mins)")
 plt.ylabel("Movie Revenue (Millions)")
 plt.title("Movie Runtime vs. Movie Revenue (2008-2018)")
-plt.xticks(np.arange(0, 300, 60))
+plt.xticks(np.arange(60, 300, 60))
 plt.yticks(np.arange(0, 3500, 500))
+print(f"The P-Value is: {'{:0,.8f}'.format(pvalue)}")
 plt.show()
 
 save_file = os.path.join("..", "Images", "Movie Runtime vs Rev Scatter.png")
 fig.savefig(save_file)
 
+
+# %%
+# Remove records with null or zero values for IMDB Rating
+sort_time["IMDB Rating"] = sort_time["IMDB Rating"].fillna(0)
+sort_time["IMDB Rating"] = sort_time["IMDB Rating"].astype(float)
+sort_time = sort_time[sort_time["IMDB Rating"] > 0]
+
+# Create a Scatter plot to show correlation between movie runtime and IMDB Rating
+x = sort_time["Runtime (Min)"]
+y = sort_time["IMDB Rating"]
+
+fig = plt.figure(figsize=(10, 7))
+plt.scatter(x, y)
+(slope, intercept, rvalue, pvalue, stderr) = st.linregress(x, y)
+regress_values = x * slope + intercept
+plt.plot(x, regress_values, "r-")
+plt.ylabel("IMDB Movie Rating")
+plt.xlabel("Movie Runtime (Mins)")
+plt.title("Movie Runtime vs. IMDB Movie Rating")
+plt.xticks(np.arange(60, 300, 60))
+print(f"The P-Value is: {'{:0,.8f}'.format(pvalue)}")
+plt.show()
+
+save_file = os.path.join("..", "Images", "Movie Runtime vs Rating Scatter.png")
+fig.savefig(save_file)
 
 # %%
 
